a7ecbcca6e8732fdcc7bac03cd2297a82e9298eb
David Lanham <david@fireworkx.co.za>
2021-11-23 14:29:44 +0200

    Added accordion functionality for contact us page

--- assets/penta-global.js
+++ assets/penta-global.js
@@ -1,39 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
 	// Modals
-	const modal = {
-		open: false,
-		type: "",
-		elem: "",
-		buttons: document.querySelectorAll("a[data-modal]"),
-		closeElems: document.querySelectorAll('[data-modal="close-modal"]'),
-		overlay: document.querySelector(".penta-modal-overlay"),
-		openModal(type) {
-			modal.open = true;
-			modal.type = type;
-			modal.elem = document.querySelector(`.penta-modal#${type}`);
-			modal.elem.classList.add("penta-show");
-			modal.overlay.classList.add("penta-show");
+	const penta = {
+		modal: {
+			open: false,
+			type: "",
+			elem: "",
+			buttons: document.querySelectorAll("a[data-modal]"),
+			closeElems: document.querySelectorAll('[data-modal="close-modal"]'),
+			overlay: document.querySelector(".penta-modal-overlay"),
+			openModal(type) {
+				this.open = true;
+				this.type = type;
+				this.elem = document.querySelector(`.penta-modal#${type}`);
+				this.elem.classList.add("penta-show");
+				this.overlay.classList.add("penta-show");
+			},
+			closeModal() {
+				this.open = false;
+				this.type = "";
+				this.elem.classList.remove("penta-show");
+				this.overlay.classList.remove("penta-show");
+				this.elem = "";
+			},
+			init() {
+				if (this.buttons) {
+					this.buttons.forEach((button) => {
+						button.addEventListener("click", () => {
+							this.openModal(button.dataset.modal);
+						});
+					});
+				}
+				if (this.closeElems) {
+					this.closeElems.forEach((elem) => {
+						elem.addEventListener("click", () => {
+							this.closeModal();
+						});
+					});
+				}
+			}
 		},
-		closeModal() {
-			modal.open = false;
-			modal.type = "";
-			modal.elem.classList.remove("penta-show");
-			modal.overlay.classList.remove("penta-show");
-			modal.elem = "";
+		accordions: {
+			buttons: document.querySelectorAll(".penta-accordion"),
+			init() {
+				if (this.buttons) {
+					this.buttons.forEach((button) => {
+						button.addEventListener("click", () => {
+							if (button.nextElementSibling.classList.contains("penta-accordion-content"))
+								button.nextElementSibling.classList.toggle("penta-show");
+						});
+					});
+				}
+			}
+		},
+		init() {
+			this.modal.init();
+			this.accordions.init();
 		}
 	};
-	if (modal.buttons) {
-		modal.buttons.forEach((button) => {
-			button.addEventListener("click", () => {
-				modal.openModal(button.dataset.modal);
-			});
-		});
-	}
-	if (modal.closeElems) {
-		modal.closeElems.forEach((elem) => {
-			elem.addEventListener("click", () => {
-				modal.closeModal();
-			});
-		});
-	}
+	penta.init();
 });
