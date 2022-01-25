5b74628e8ce20d1f139470c8d600fceb90e6219d
David Lanham <david@fireworkx.co.za>
2022-01-25 09:36:40 +0200

    Restored code from 24th of December

--- assets/penta-global.js
+++ assets/penta-global.js
@@ -12,40 +12,53 @@ document.addEventListener("DOMContentLoaded", () => {
 				dealership: "",
 				select: ""
 			},
+			gallery: {
+				index: 0
+			},
 			buttons: document.querySelectorAll("[data-modal]"),
 			closeElems: document.querySelectorAll("[data-close-modal]"),
 			overlay: document.querySelector(".penta-modal-overlay"),
 			body: document.querySelector("body"),
-			openModal({ type, brand, uid, dealership }) {
+			openModal({ type, brand, uid, dealership }, e) {
 				return new Promise((resolve, reject) => {
 					this.open = true;
 					this.body.classList.add("modal-open");
-					this.form = {
-						elem: document.querySelector(`.penta-modal#${type}`),
-						type: type,
-						brand: brand,
-						uid: uid,
-						dealership: dealership,
-						select: document.querySelector(`.penta-modal#${type} select[name="Branch"]`)
-					};
-					this.form.elem.classList.add("penta-show");
 					this.overlay.classList.add("penta-show");
-					return resolve(({ elem, select, brand, dealership, uid } = this.form));
+					if (type == "gallery-modal") {
+						this.type = "gallery-modal";
+					}
+					{
+						this.type = type;
+						this.form = {
+							elem: document.querySelector(`.penta-modal#${type}`),
+							type: type,
+							brand: brand,
+							uid: uid,
+							dealership: dealership,
+							select: document.querySelector(`.penta-modal#${type} select[name="Branch"]`)
+						};
+						this.form.elem.classList.add("penta-show");
+						return resolve(({ elem, select, brand, dealership, uid } = this.form));
+					}
 				});
 			},
 			closeModal() {
 				this.open = false;
 				this.body.classList.remove("modal-open");
-				this.form.elem.classList.remove("penta-show");
 				this.overlay.classList.remove("penta-show");
-				this.form = {
-					elem: "",
-					type: "",
-					brand: "",
-					uid: "",
-					dealership: "",
-					select: ""
-				};
+				if (type == "gallery-modal") {
+				}
+				{
+					this.form.elem.classList.remove("penta-show");
+					this.form = {
+						elem: "",
+						type: "",
+						brand: "",
+						uid: "",
+						dealership: "",
+						select: ""
+					};
+				}
 			},
 			init() {
 				if (this.buttons) {
@@ -59,7 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
 						};
 						button.addEventListener("click", () => {
 							this.openModal(({ type, brand, uid, dealership } = data)).then(() => {
-								if (!uid) {
+								if (!uid && type != "gallery-modal") {
 									penta.forms.buildSelect(elem, select, brand, dealership, uid);
 								}
 							});
@@ -70,19 +83,6 @@ document.addEventListener("DOMContentLoaded", () => {
 					this.closeElems.forEach((elem) => {
 						elem.addEventListener("click", () => {
 							this.closeModal();
-						});
-					});
-				}
-			}
-		},
-		accordions: {
-			buttons: document.querySelectorAll(".penta-accordion"),
-			init() {
-				if (this.buttons) {
-					this.buttons.forEach((button) => {
-						button.addEventListener("click", () => {
-							if (button.nextElementSibling.classList.contains("penta-accordion-content"))
-								button.nextElementSibling.classList.toggle("penta-show");
 						});
 					});
 				}
@@ -151,28 +151,34 @@ document.addEventListener("DOMContentLoaded", () => {
 		},
 		init() {
 			this.modal.init();
-			this.accordions.init();
 			this.forms.init();
 		}
 	};
 	// Form constructor
 	function Form(form) {
+		const submitButton = form.querySelector(".penta-submit-button");
 		this.sendForm = function ({ uid }) {
 			let formData = new FormData(form);
+			// Form specific payloads
+			switch (form.id) {
+				case "test-drive-form":
+					formData.set(
+						"Message",
+						[
+							`Message: ${formData.get("Message")}\n`,
+							`Preferred Date: ${formData.get("PreferredDate")}\n`,
+							`Preferred Time: ${formData.get("PreferredTime")}\n`
+						].join("")
+					);
+					formData.delete("PreferredDate");
+					formData.delete("PreferredTime");
+					break;
+				default:
+			}
 			formData.append("Type", form.dataset.formType);
 			formData.append("URL", window.location.href);
 			formData.set("StockListApiId", uid);
-			
-			formData.set(
-				"Message",
-				`Message: ${formData.get("Message")}\nMake: ${formData.get("Make")}\nModel:  ${formData.get(
-					"Model"
-				)}\nTest Drive - Preferred Date:  ${formData.get(
-					"PreferredDate"
-				)}\nTest Drive - Preferred Time:  ${formData.get("PreferredTime")}\n`
-			);
-			formData.delete("CanopyFeatures");
-			
+			formData.delete("Branch");
 			formDataJson = JSON.stringify(Object.fromEntries(formData));
 			fetch(penta.forms.api, {
 				method: "POST",
@@ -190,6 +196,7 @@ document.addEventListener("DOMContentLoaded", () => {
 						penta.forms.showFeedback("success");
 						penta.modal.closeModal();
 					}
+					submitButton.disabled = false;
 				})
 				.catch(() => {
 					penta.forms.showFeedback("error");
@@ -199,11 +206,10 @@ document.addEventListener("DOMContentLoaded", () => {
 			form.reset();
 		};
 		this.init = function () {
-			const submitButton = form.querySelector(".penta-submit-button");
 			submitButton.addEventListener("click", () => {
-				window.dataLayer.push({'event_name': 'enquiry_submit'});
-
+				window.dataLayer.push({ event_name: "enquiry_submit" });
 				if (form.checkValidity()) {
+					submitButton.disabled = true;
 					this.sendForm(({ uid } = penta.modal.form));
 				} else {
 					form.reportValidity();
